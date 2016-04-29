def66250ed5b751edd183764b0d47cc097d5b293
Roel Hogervorst <hogervorst.rm@gmail.com>
2016-04-29 16:25:20 +0200

    added tests for searches projectstatusbadge function

--- R/searches.R
+++ R/searches.R
@@ -1,11 +1,11 @@
-# searchfortravis
+
 # 
-description <- readLines("DESCRIPTION")
-grep()
-
-list <- list.files(,pattern = "\\.*")
-
-# travis checks
+#' Travisbadge creates travis badge.
+#'
+#' @return link to travis image
+#' @export
+#'
+#' @examples
 travisbadge <- function(){
     if(file.exists(".travis.yml")) {
         branch <- attr(git2r::branches(flags = "local"), "names")
@@ -20,34 +20,17 @@ travisbadge <- function(){
         }
 }
 
-#remote <-git2r::remote_url(git2r::repository())
-#ghaccount <- "RMHogervorst"
-#ghrepo <- "badgecreatr"
-
-
-#git2r::branch_remote_url()
-
-# [![Build Status](https://travis-ci.org/RMHogervorst/
-#                      heisertransform.svg?branch=develop)]
-# (https://travis-ci.org/RMHogervorst/heisertransform)
-
-# 
-# if (grepl("^https", x)) {
-#     # https://github.com/hadley/devtools.git
-#     re <- "github.com/(.*?)/(.*)\\.git"
-# } else if (grepl("^git", x)) 
-#     
-# Finding readme.
-readme <- readLines("README.Rmd")
-grep("travis-ci.org", readme)
-
-http://www.repostatus.org/badges/latest/concept_md.txt# devtools::use_github_links()
-
-http://www.repostatus.org/badges/latest/active_md.txt
-
 # add project status
 # 
-status<- "wip"
+
+#' Title
+#'
+#' @param status 
+#'
+#' @return
+#' @export
+#'
+#' @examples projectstatusbadge("unsupported")
 projectstatusbadge <- function(status){
     name <- c("concept", "wip", "suspended", "abandoned", "active", "inactive", "unsupported")
     if(!status %in% name)stop("status needs to be one of concept, wip, suspended, abandoned, active, inactive, unsupported")
@@ -56,20 +39,9 @@ projectstatusbadge <- function(status){
     repostatus <- gsub("â€“", "-", repostatus)
     repostatus
 }
-projectstatusbadge("unsupported")
 
-## readLines(con = paste0("http://www.repostatus.org/badges/latest/",name[7], "_md.txt" ))
-# this works
-# 
-# add_badges(status = active, )
-# 
-# tests: 
-# status<- "kip" should give error
-# Error: status needs to be one of concept, wip, suspended, abandoned, active, inactive, unsupported
-# projectstatusbadge("wip") should give 
-# "[![Project Status: WIP - Initial development is in progress, 
-# but there has not yet been a stable, usable release suitable for the 
-# public.](http://www.repostatus.org/badges/latest/wip.svg)](http://www.repostatus.org/#wip)"
+
+
 # 
 
 # adds the github links. 
---
+++ tests/testthat/test_searches.R
@@ -0,0 +1,28 @@
+context("general function of searches")
+# tests: 
+# status<- "kip" should give error
+# Error: status needs to be one of concept, wip, suspended, abandoned, active, inactive, unsupported
+# projectstatusbadge("wip") should give 
+# "[![Project Status: WIP - Initial development is in progress, 
+# but there has not yet been a stable, usable release suitable for the 
+# public.](http://www.repostatus.org/badges/latest/wip.svg)](http://www.repostatus.org/#wip)"
+
+test_that("error when incorrect status",{
+    expect_error(
+        projectstatusbadge("kip"),
+        regexp = "status needs to be one of concept, wip, suspended, abandoned, active, inactive, unsupported"
+        )  
+})
+test_that("internetconnection works",{
+    expect_equal(readLines("http://www.repostatus.org/#wip")[1], "<!doctype html>" )
+})
+
+test_that("all status options work"{
+    expect_output(projectstatusbadge("concept"),regexp = "\\[Project Status: Concept")
+    expect_output(projectstatusbadge("wip"),regexp = "Project Status: WIP")
+    expect_output(projectstatusbadge("suspended"),regexp = "Project Status: Suspended")
+    expect_output(projectstatusbadge("abandoned"),regexp = "Project Status: Abandoned")
+    expect_output(projectstatusbadge("active"),regexp = "Project Status: Active")
+    expect_output(projectstatusbadge("inactive"),regexp = "Project Status: Inactive")
+    expect_output(projectstatusbadge("unsupported"),regexp = "Project Status: Unsupported")
+})
