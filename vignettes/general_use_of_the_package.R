9a290d0d0c1afd70a127cbcff5ac9b5010f04bf5
Roel Hogervorst <hogervorst.rm@gmail.com>
2017-06-11 10:19:47 +0200

    added namespace

--- vignettes/general_use_of_the_package.R
+++ vignettes/general_use_of_the_package.R
@@ -5,3 +5,16 @@ badge_projectstatus("wip") # any of concept, wip, suspended, abandoned, active, 
 ## ----scen 1--------------------------------------------------------------
 minimal_badges(status = "concept", licence = "MIT",date = "2017-05-17")
 
+## ----scen 2, eval=FALSE--------------------------------------------------
+#  dynamic_badges_minimal(licence = "MIT", travisfile = TRUE, codecov = TRUE)
+
+## ------------------------------------------------------------------------
+
+
+## ---- eval=FALSE---------------------------------------------------------
+#  # is not implemented yet.
+#  place_badge_groups(
+#      CI_status = c("travis", "rwin"),
+#      versions = c("CRAN", "github"),
+#      stats = c("downloads, gh_stars"))
+
